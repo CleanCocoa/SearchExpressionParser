38f0d0dc7bb9675a1bc2802455b7ce173398f8fb
Christian Tietze <christian.tietze@gmail.com>
2018-04-07 13:08:10 +0200

    parse sequence of many phrases as AND-connected

--- SearchExpressionParser/Parsing/Parser.swift
+++ SearchExpressionParser/Parsing/Parser.swift
@@ -2,6 +2,8 @@
 
 public struct Parser {
 
+    internal typealias Result = Either<Expression, ParseError>
+
     public let tokens: [Token]
 
     public init(tokens: [Token]) {
@@ -12,12 +14,30 @@ public struct Parser {
 
         let tokenBuffer = TokenBuffer(tokens: tokens)
 
-        guard let first = tokenBuffer.popToken() else { return AnythingNode() }
+        guard let next = parseExpression(tokenBuffer: tokenBuffer) else { return AnythingNode() }
 
-        if let next = tokenBuffer.popToken() {
-            return AndNode(ContainsNode(token: first), ContainsNode(token: next))
+        switch next {
+        case .error(let error):
+            throw error
+        case .value(let expression):
+            return expression
         }
-
-        return ContainsNode(token: first)
     }
+
+    private func parseExpression(tokenBuffer: TokenBuffer) -> Result? {
+
+        guard let current = tokenBuffer.popToken() else { return nil }
+        guard let next = parseExpression(tokenBuffer: tokenBuffer) else { return .value(ContainsNode(token: current)) }
+
+        switch next {
+        case .error(_):
+            return next
+        case .value(let nextExpression):
+            return .value(AndNode(ContainsNode(token: current), nextExpression))
+        }
+    }
+}
+
+internal enum ParseError: Error {
+    case expectedTokenAtExpressionStart
 }
--- SearchExpressionParserTests/ParserTests.swift
+++ SearchExpressionParserTests/ParserTests.swift
@@ -23,4 +23,37 @@ class ParserTests: XCTestCase {
 
         XCTAssertEqual(expression, AndNode(ContainsNode("foo"), ContainsNode("bar")))
     }
+
+    func testExpression_3Phrases() {
+        let tokens: [Token] = [Phrase("foo"), Phrase("bar"), Phrase("baz")]
+        guard let expression = XCTAssertNoThrows(try Parser(tokens: tokens).expression()) else { return }
+
+        XCTAssertEqual(
+            expression,
+            AndNode(
+                ContainsNode("foo"),
+                AndNode(
+                    ContainsNode("bar"),
+                    ContainsNode("baz"))))
+    }
+
+    func testExpression_6Phrases() {
+        let tokens: [Token] = [Phrase("1"), Phrase("2"), Phrase("3"), Phrase("4"), Phrase("5"), Phrase("6")]
+        guard let expression = XCTAssertNoThrows(try Parser(tokens: tokens).expression()) else { return }
+
+        XCTAssertEqual(
+            expression,
+            AndNode(
+                ContainsNode("1"),
+                AndNode(
+                    ContainsNode("2"),
+                    AndNode(
+                        ContainsNode("3"),
+                        AndNode(
+                            ContainsNode("4"),
+                            AndNode(
+                                ContainsNode("5"),
+                                ContainsNode("6")))))))
+
+    }
 }
