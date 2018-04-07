5f4ed9043fb224be4072052ae4181f105ce930d7
Christian Tietze <christian.tietze@gmail.com>
2018-04-07 08:10:34 +0200

    Initial commit

---
+++ SearchExpressionParser/SearchExpressionParser.h
@@ -0,0 +1,19 @@
+//
+//  SearchExpressionParser.h
+//  SearchExpressionParser
+//
+//  Created by Christian Tietze on 07.04.18.
+//  Copyright © 2018 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SearchExpressionParser.
+FOUNDATION_EXPORT double SearchExpressionParserVersionNumber;
+
+//! Project version string for SearchExpressionParser.
+FOUNDATION_EXPORT const unsigned char SearchExpressionParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SearchExpressionParser/PublicHeader.h>
+
+
